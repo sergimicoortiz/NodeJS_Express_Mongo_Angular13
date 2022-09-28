aea6ac4ba54cb40263eda548005b311c78c6f528
JuanLuis <juanluislopezdaw@gmail.com>
2022-09-28 21:04:50 +0200

    carousel autoSlide added and END

--- frontend/src/app/shared/carousel/carousel.component.ts
+++ frontend/src/app/shared/carousel/carousel.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Category } from '../../core'
 
 @Component({
@@ -6,15 +6,45 @@ import { Category } from '../../core'
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.css']
 })
-export class CarouselComponent {
+export class CarouselComponent implements OnInit {
 
   @Input() category: Category[] = [];
   @Input() indicators = true;
+  @Input() controls = true;
+  @Input() autoSlide = false;
+  @Input() slideInterval = 3000;
 
   selectIndex = 0;
+
+  ngOnInit(): void {
+    if (this.autoSlide) {
+      this.autoSlideImages();
+    }
+  }
+
+  autoSlideImages(): void {
+    setInterval(()=>{
+      this.onNextClick();
+    }, this.slideInterval);
+  }
 
   selectImage(index: number): void {
     this.selectIndex = index;
   }
 
+  onPrevClick(): void {
+    if (this.selectIndex === 0) {
+      this.selectIndex = this.category.length - 1;
+    } else {
+      this.selectIndex--;
+    }
+  }
+
+  onNextClick(): void {
+    if (this.selectIndex === this.category.length - 1) {
+      this.selectIndex = 0;
+    } else {
+      this.selectIndex++;
+    }
+  }
 }
